0ef6ab93995d915a141a04e90469293f09e4bbc2
Christian Irack <ingeniovisual@hotmail.com>
2016-03-13 19:53:34 -0600

    Estable dos secciones

--- public/js/controller.js
+++ public/js/controller.js
@@ -7,11 +7,7 @@ function generateSlider(num){
        swiper = new Swiper('.swiper-container', {
             spaceBetween: 0,
             slidesPerView: 1,
-            centeredSlides: false,
-            slideToClickedSlide: true,
-            grabCursor: true,
             effect:'slide',
-            roundLengths:true,
             nextButton: '.swiper-button-next',
             prevButton: '.swiper-button-prev',
             scrollbar: '.swiper-scrollbar',
@@ -22,7 +18,7 @@ function generateSlider(num){
 
 var sections = {
   home:['homeSection.html'],
-  vinos:['homeSection.html','homeSection2.html']
+  aboutUs:['aboutUsSection.html','aboutUsSection2.html']
 }
 
 var openSection = {
@@ -32,19 +28,24 @@ var openSection = {
   },
   openUrl: function(getSection){
    var selectSection = sections[getSection];
+   $('.swiper-wrapper').empty();
      for(var value in selectSection){
       // console.log(selectSection[value]);
        var file = selectSection[value];
+
        var hash = this.timer()+value;
-       $('.swiper-wrapper').empty();
+       console.log(hash+'/'+file);
+
        $('.swiper-wrapper').append('<div id="'+hash+'" class="swiper-slide"></div>');
-       $('#'+hash).load('sections/'+file+'?cache='+hash);
+       $('#'+hash).load('sections/'+file+'?cache='+hash, function(){
+         reloadCSSElements();
+       });
+
      };
      if(swiper){
        swiper.destroy(true, true);
      };
      generateSlider($(".swiper-wrapper").length);
-
   }
 };
 
@@ -52,25 +53,71 @@ var openSection = {
 $(function(){
 //alert($(window).width()+'/'+$(window).height());
       // generateSlider();
-       openSection.openUrl('home');
-      orientarionChange();
-      windowResize();
-      animation();
-})
-
-function animation(){
-  $(".logo").animateSprite({
-    fps: 12,
-    animations: {
-        iniciar: [0, 1, 2, 3, 4, 5, 6, 7,8,9]
-    },
-    loop: true,
-    complete: function(){
-        // use complete only when you set animations with 'loop: false'
-        alert("animation End");
-    }
+    openSection.openUrl('home');
+    orientarionChange();
+    windowResize();
+    closeMenu(false);
+    openMenu(false);
+    sectionsActivate();
+    scrollDetect();
 });
+
+function scrollDetect(){
+  var lastScrollTop = 0;
+  $(window).scroll(function(event){
+     var st = $(this).scrollTop();
+     if (st > lastScrollTop){
+         console.log("abajo");
+     } else {
+        console.log("arriba");
+     }
+     lastScrollTop = st;
+  });
 }
+
+function sectionsActivate(){
+
+  $('.homeClass').click(function(){
+     openSection.openUrl('home');
+     closeMenu(true);
+  });
+  $('.aboutUsClass').click(function(){
+     openSection.openUrl('aboutUs');
+      closeMenu(true);
+  });
+}
+
+function closeMenu(mode){
+  if(mode){
+      closeMenuAnim();
+  }else{
+    $(".close").click(function(){
+      closeMenuAnim();
+    });
+  }
+}
+function closeMenuAnim(){
+      TweenMax.to($(".fullMenuLinks"), .6, {left:"-"+$(".fullMenuLinks").outerWidth()+"px", ease: Power4.easeIn});
+      TweenMax.to($(".fullMenu"), 1, {opacity:"0",autoAlpha: 1, display:'none', delay:.2, ease: Power4.easeIn});
+}
+function openMenu(mode){
+  if(mode){
+      openMenuAnim();
+     // $(".seccionLink").addClass('AexitMenu2');
+  }else{
+    $("#topMenu").click(function(){
+      openMenuAnim();
+     // $(".seccionLink").addClass('AexitMenu2');
+    });
+  }
+}
+
+function openMenuAnim(){
+      $(".fullMenuLinks,.fullMenu").css('display','block');
+      TweenMax.fromTo($(".fullMenuLinks"), 1, {left:"-"+$(".fullMenuLinks").outerWidth()+"px"},{left:"0px", delay:.5, ease: Power4.easeIn});
+      TweenMax.to($(".fullMenu"), 1.5, {opacity:"1", ease: Power4.easeIn})
+}
+
 //Fix problem sizes percent
 var monitorElements = ['backgroundHome'];
 ///////
@@ -83,11 +130,17 @@ function orientarionChange(){
   window.addEventListener("orientationchange", function() {
 	// Announce the new orientation number
       	if(window.orientation==0){
-        	reloadCSSElements();
+        	//reloadCSSElements();
       	}else{
-        	  reloadCSSElements();
+        	 // reloadCSSElements();
         	//alert('landscape');
+        	closeMenu(true);
       	}
+      	setTimeout(function(){
+        	reloadCSSElements();
+      	}, 100);
+
+
     }, false);
 };
 function reloadCSSElements(){
@@ -96,5 +149,5 @@ function reloadCSSElements(){
           	$('.'+refreshObject).parent().css({'width':'100%','height':'100%'});
           	//alert($('.'+refreshObject).parent().attr('class'));
         	}
-        	console.log('ok');
+        	//console.log('ok');
 }
